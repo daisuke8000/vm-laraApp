149fd3162ededdb84d402e80e153db8c44efb619
daisuke8000 <daisuke12108000@gmail.com>
2020-11-29 14:41:39 +0900

    create table tests testusers

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Daisuke
                 </div>
 
                 <div class="links">
